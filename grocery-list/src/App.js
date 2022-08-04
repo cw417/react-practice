95078ed94a2404a448895c5f53a7dbf8d836de80
cw417 <chriswhitlock417@gmail.com>
2022-08-04 14:04:56 -0400

    Restart project from ground up

---
+++ grocery-list/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import GroceryList from './GroceryList';
+
+function App() {
+  return (
+    <>
+      <GroceryList />
+    </>
+  );
+}
+
+export default App;
---
+++ grocery-list/src/GroceryList.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import Grocery from './Grocery';
+
+export default function GroceryList() {
+  return (
+    <div>GroceryList</div>
+  )
+}
